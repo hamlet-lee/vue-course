d9b8e00c1b8430305a25a04ca3e1ed8598da4a4d
Hamlet Lee <lisn@rd.netease.com>
2019-11-11 17:38:54 +0800

    MOD: 演示import/export，以及代码风格检查

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { test1 } from './test'
 
 Vue.config.productionTip = false
 
+// aaaa
+
+
+// bbb
+
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="我是Hamlet，今天开始学习前端工程化！"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="我是Hamlet，今天开始学习前端工程化！" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+
+import { test1 } from "@/test";
 
 export default {
-  name: 'home',
+  name: "home",
+
   components: {
     HelloWorld
   }
-}
+};
 </script>
